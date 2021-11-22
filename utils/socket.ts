ebaf3cd38a273d7de87f7fbe9cf3f9bbc48f0b01
_StarChaser <32775520+ST4RCHASER@users.noreply.github.com>
2021-11-23 01:02:25 +0700

    Update socket.ts

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,7 +1,8 @@
 import socketIOClient, { Socket } from "socket.io-client";
 let socket: any = undefined;
 export function getSocket(): Socket {
-    const ENDPOINT = "http://localhost:8081";
+    const ENDPOINT = "https://tenshi-backend.herokuapp.com";
+    // const ENDPOINT = "http://localhost:8081";
     if (socket && socket.connected) return socket;
     socket = socketIOClient(ENDPOINT);
     return socket;
